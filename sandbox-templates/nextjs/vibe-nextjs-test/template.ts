0e803a3f6986618c6220a9705f6ffab86e0b7a7e
mrlongruoi <mrlong01012020@gmail.com>
2025-10-21 12:17:35 +0700

    fixxxlai

--- sandbox-templates/nextjs/vibe-nextjs-test/template.ts
+++
@@ -1,8 +0,0 @@
-import { Template } from "@e2b/sdk"
-
-// Type assertion as temporary fix
-export const template = new (Template as any )({
-  name: "vibe-nextjs-test",
-  runtime: "node",
-  entrypoint: "index.js",
-})
